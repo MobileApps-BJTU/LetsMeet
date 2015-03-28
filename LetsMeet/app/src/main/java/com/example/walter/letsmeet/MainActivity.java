8e25113996684e1dd34cf18e4553d25569791a32
12301081-Walter <12301081@bjtu.edu.cn>
2015-03-28 22:35:50 +0800

    Prototype Application
    for Android Studio

---
+++ LetsMeet/app/src/main/java/com/example/walter/letsmeet/MainActivity.java
@@ -0,0 +1,158 @@
+package com.example.walter.letsmeet;
+
+import android.content.SharedPreferences;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+
+public class MainActivity extends ActionBarActivity implements MainFragment.OnFragmentInteractionListener{
+
+    private static final String ACTIVITY = "activity";
+    private static final String NAME = "name";
+    private static final String LOCATION = "location";
+    private static final String DATE = "date";
+    private static final String NUMBER = "number";
+    private static final String TYPE = "type";
+
+    private SharedPreferences savedActivities,savedAnActivity;
+    private SharedPreferences.Editor editor;
+    private ArrayList<String> activities;
+    private Map<String,String> activityNames;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        savedActivities = getSharedPreferences(ACTIVITY,MODE_PRIVATE);
+
+        activities = new ArrayList<String>(savedActivities.getAll().keySet());
+        activityNames = new HashMap<String,String>();
+
+        for (String s:activities){
+            activityNames.put(s,savedActivities.getString(s,""));
+        }
+
+        setExample();
+
+        getFragmentManager().beginTransaction()
+                .add(R.id.fragment_holder,new MainFragment(getData()))
+                .commit();
+    }
+
+    public ArrayList<Map<String,Object>> getData(){
+
+        ArrayList<Map<String,Object>> list = new ArrayList<Map<String,Object>>();
+        Map<String,Object> map;
+
+        for (String s: activities){
+            savedAnActivity = getSharedPreferences(s,MODE_PRIVATE);
+            map = new HashMap<String,Object>();
+
+            map.put(NAME,savedAnActivity.getString(NAME,""));
+            map.put(LOCATION,savedAnActivity.getString(LOCATION,"地点待定"));
+            map.put(DATE,savedAnActivity.getString(DATE,"时间待定"));
+            map.put(NUMBER,savedAnActivity.getInt(NUMBER, 2));
+
+            int judge = savedAnActivity.getInt(TYPE,5);
+            if(judge == 0){
+                map.put(TYPE,"WFY");
+            }else if (judge == 1){
+                map.put(TYPE,"ME");
+            }else if (judge == 2){
+                map.put(TYPE,"LM");
+            }else {
+                Toast.makeText(this,"该条类型未定，不予显示",Toast.LENGTH_LONG).show();
+                continue;
+            }
+
+            list.add(map);
+        }
+
+        return list;
+    }
+
+    public void setExample(){
+
+            String str = "第一个活动的例子";
+        int temp = 1;
+        if (activityNames.containsValue(str)){
+            Toast.makeText(this,"活动名称已经存在",Toast.LENGTH_LONG).show();
+        }else{
+            while(activities.contains("acti_"+String.valueOf(temp))){
+                temp++;
+            }
+            editor = savedActivities.edit();
+            editor.putString("acti_"+String.valueOf(temp),str);
+            editor.commit();
+            activities.add("acti_" + String.valueOf(temp));
+            activityNames.put("acti_" + String.valueOf(temp),str);
+            savedAnActivity = getSharedPreferences("acti_"+String.valueOf(temp),MODE_PRIVATE);
+            editor = savedAnActivity.edit();
+            editor.putString(NAME,str);
+            editor.putString(LOCATION,"西直门凯德mall");
+            editor.putString(DATE,"四月3号");
+            editor.putInt(NUMBER,5);
+            editor.putInt(TYPE,1);
+
+            if (editor.commit()){
+                Toast.makeText(this,"数据写入成功",Toast.LENGTH_LONG).show();
+            }else {
+                Toast.makeText(this,"数据写入失败",Toast.LENGTH_LONG).show();
+            }
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClickCreateButton() {
+        getFragmentManager().beginTransaction()
+                .replace(R.id.fragment_holder,new BlankFragment())
+                .addToBackStack(null)
+                .commit();
+    }
+
+    @Override
+    public void clickListItem() {
+
+    }
+
+    @Override
+    public void onBackPressed(){
+
+        if(getFragmentManager().getBackStackEntryCount() > 0){
+            getFragmentManager().popBackStack();
+        }else{
+            super.onBackPressed();
+        }
+    }
+}
---
+++ LetsMeet/app/src/main/java/com/example/walter/letsmeet/MainFragment.java
@@ -0,0 +1,108 @@
+package com.example.walter.letsmeet;
+
+import android.app.Activity;
+import android.app.ListFragment;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link MainFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ */
+public class MainFragment extends ListFragment {
+
+    private OnFragmentInteractionListener mListener;
+    private SimpleAdapter mAdapter;
+    private ArrayList<Map<String,Object>> allInformation;
+
+    public MainFragment() {
+        // Required empty public constructor
+    }
+    public MainFragment(ArrayList<Map<String,Object>> information){
+        allInformation = information;
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View myView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        mAdapter = new SimpleAdapter(getActivity(),allInformation,R.layout.list_items,
+                new String[]{"name","location","date","number","type"},
+                new int[]{R.id.actiNameTextView,R.id.actiLocatTextView,R.id.actiDateTextView,R.id.actiPeopleTextView,R.id.actiTypeTextView });
+
+        setListAdapter(mAdapter);
+        Button createButton = (Button)myView.findViewById(R.id.CreateButton);
+        createButton.setOnClickListener(createButtonListener);
+        // Inflate the layout for this fragment
+        return myView;
+    }
+
+    @Override
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onListItemClick(ListView l,View v,int position,long id) {
+
+        if (mListener != null) {
+            mListener.clickListItem();
+        }
+    }
+
+    public OnClickListener createButtonListener = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if (mListener!=null){
+                mListener.onClickCreateButton();
+            }
+        }
+    };
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            mListener = (OnFragmentInteractionListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        public void onClickCreateButton();
+        public void clickListItem();
+    }
+
+}
